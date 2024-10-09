e9aa92f3ab483749d20fc8bfb471093b01e1db19
real-turki-marzoqi <turki.marzoqi.social@gmail.com>
2024-10-09 13:08:07 +0300

    test 1.0

--- services/orderService.js
+++ services/orderService.js
@@ -204,20 +204,24 @@ exports.checkoutSession = asyncHandler(async (req, res, next) => {
 });
 
 
-exports.webhookCheckout = asyncHandler(async(req,res,next)=>{
+
+exports.webhookCheckout = asyncHandler(async (req, res, next) => {
   const sig = req.headers['stripe-signature'];
 
   let event;
 
   try {
-    event = stripe.webhooks.constructEvent(request.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
+    event = stripe.webhooks.constructEvent(req.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
   } catch (err) {
     return res.status(400).send(`Webhook Error: ${err.message}`);
-    
-  }
-  if(event.type === checkout.session.completed){
-
-    console.log('Create Order Here')
   }
 
-})
+  
+  if (event.type === 'checkout.session.completed') {
+    console.log('Create Order Here');
+   
+  }
+
+  res.status(200).json({ received: true });
+});
+
